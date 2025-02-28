842b380b6f9c8eb6c469e007ebfce602d34a8982
chulsu0012 <chulsu0012@gmail.com>
2025-02-28 10:52:25 +0900

    update: 코드 업데이트

--- frontend/src/pages/Landing/Hero.tsx
+++ frontend/src/pages/Landing/Hero.tsx
@@ -21,7 +21,7 @@ export default function Hero() {
     <div className="container center flex-col gap-10">
       <div className="flex flex-col justify-center items-center gap-3">
         <Title>약속시간</Title>
-        <div className="text-xl font-semibold text-myDarkGray">나를 위한 맞춤형 복약 관리 서비스</div>
+        <div className="text-xl font-semibold text-myDarkGray select-none">나를 위한 맞춤형 복약 관리 서비스</div>
       </div>
       <RedirectButton
         path="info/input/name"
